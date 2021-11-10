04d67d243d8e0ebd741b105dc3ce3bf7fe7c02af
Gertjan <info@esolutions-it.nl>
2021-11-10 16:51:40 +0100

    Athorization

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,7 +28,16 @@
                         
                         <p class="mb-2">{{ $post->body }}</p>
 
+                        @can('delete', $post)
+                            <form action="{{ route('posts.destroy', $post) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-blue-500">Delete</button>
+                            </form>
+                        @endcan
+
                         <div class="flex items-center">
+                            @auth
                             @if(!$post->likedBy(auth()->user()))
                                 <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
                                     @csrf
@@ -41,6 +50,7 @@
                                     <button type="submit" class="text-blue-500">Unlike</button>
                                 </form>
                             @endif
+                            @endauth
                             <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                         </div>
                     </div>
